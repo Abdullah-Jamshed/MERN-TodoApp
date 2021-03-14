c1cecfdd8c944876cb85c2cb3f1a25114ed06c9e
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-03-14 22:54:33 +0500

    updates

--- client/src/Routes/Auth.js
+++ client/src/Routes/Auth.js
@@ -4,49 +4,62 @@ import { Container, Grid, Box, makeStyles, Typography } from "@material-ui/core"
 const Auth = () => {
   const classes = useStyles();
   return (
-    <Container disableGutters={true} className={classes.container}>
-      <Grid container className={classes.subContainer}>
-        <Grid item xs={12} sm={4} lg={3} className={classes.gridItem1}>
-          <Typography>Todo</Typography>
-        </Grid>
+    <>
+      <Container disableGutters={true} className={classes.container}>
+        <Grid container className={classes.subContainer}>
+          <Grid item xs={12} sm={5} md={4} lg={3} className={classes.gridItem1}>
+            <Typography>Form</Typography>
+          </Grid>
 
-        {/* <Box component={Grid} className={classes.gridItem2} item sm={6} display={{ xs: "none", sm: "block" }}>
-          <h1>Auth</h1>
-        </Box> */}
-
-        <Grid component={Box} className={classes.gridItem2} item sm={8} lg={9} display={{ xs: "none", sm: "block" }}>
-          <Typography variant='h1'>Todo App</Typography>
+          <Grid component={Box} className={classes.gridItem2} item sm={7} md={8} lg={9}>
+            <Typography className={classes.heading1} variant='h1' color='textSecondary'>
+              Todo App
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  //   root: {
-  //       padding:"0px",
-  //   },
+  root: {},
   container: {
     minHeight: "100vh",
     minWidth: "100vw",
+    background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
   },
   subContainer: {
     minHeight: "100vh",
   },
   gridItem1: {
-    height: "100%",
+    minHeight: "100vh",
     borderRadius: "0px 30px 30px 0px",
-    background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
+    backgroundColor: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     [theme.breakpoints.down("xs")]: {
       backgroundColor: "#fff",
-      background:"#fff",
       borderRadius: "0px",
     },
   },
   gridItem2: {
-    height: "100%",
-    // background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
-    // textAlign: "center",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
+  heading1: {
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "80px",
+    },
   },
 }));
 
