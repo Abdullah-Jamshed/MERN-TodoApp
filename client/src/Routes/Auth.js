1133a06c380728b9dedd695f97851e051150a81e
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-03-20 22:08:52 +0500

    UPDATES

--- client/src/Routes/Auth.js
+++ client/src/Routes/Auth.js
@@ -38,12 +38,12 @@ const Auth = () => {
       {!loader ? (
         <Container disableGutters={true} className={classes.container}>
           <Grid container className={classes.subContainer}>
-            <Grid item xs={12} sm={5} md={5} lg={3} className={classes.gridItem1}>
+            <Grid item xs={12} sm={5} md={5} lg={4} className={classes.gridItem1}>
               <Form />
             </Grid>
 
-            <Grid component={Box} className={classes.gridItem2} item sm={7} md={7} lg={9}>
-              <Typography className={classes.heading1} variant='h1' color='textSecondary'>
+            <Grid component={Box} className={classes.gridItem2} item sm={7} md={7} lg={8}>
+              <Typography className={classes.heading1} variant='h1' >
                 Todo App
               </Typography>
             </Grid>
@@ -65,7 +65,9 @@ const useStyles = makeStyles((theme) => ({
   container: {
     minHeight: "100vh",
     minWidth: "100vw",
-    background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
+    // background: "linear-gradient(to right, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
+    // background: "linear-gradient(to right, #00f260, #0575e6)",
+    background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)" /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */,
   },
   subContainer: {
     minHeight: "100vh",
@@ -75,18 +77,22 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "0px 30px 30px 0px",
     backgroundColor: "#fff",
     display: "flex",
+
     justifyContent: "center",
     alignItems: "center",
     [theme.breakpoints.down("xs")]: {
       backgroundColor: "#fff",
       borderRadius: "0px",
     },
+    [theme.breakpoints.up("lg")]: {
+      padding: "0px 30px 0px 30px",
+    },
   },
   gridItem2: {
     height: "100vh",
     justifyContent: "center",
     alignItems: "center",
-    background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
+    // background: "linear-gradient(180deg, rgba(0,212,255,1) 0%, rgba(145,1,228,1) 100%)",
     [theme.breakpoints.up("sm")]: {
       display: "flex",
     },
@@ -95,6 +101,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   heading1: {
+    color:"#fff",
     [theme.breakpoints.down("sm")]: {
       fontSize: "80px",
     },
